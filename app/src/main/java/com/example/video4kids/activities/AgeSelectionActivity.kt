2b7e9d43f764b96cec94c62ede454e9dac539836
prodev9999 <prodev9999@gmail.com>
2019-06-22 22:12:49 +0800

    Added age-selection activity

---
+++ app/src/main/java/com/example/video4kids/activities/AgeSelectionActivity.kt
@@ -0,0 +1,13 @@
+package com.example.video4kids.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.video4kids.R
+
+class AgeSelectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_age_selection_screen)
+    }
+}
