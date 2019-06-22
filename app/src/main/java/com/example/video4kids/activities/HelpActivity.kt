9c1ccb03d23d5787d660bc70397f0f54f9734032
prodev9999 <prodev9999@gmail.com>
2019-06-22 22:12:58 +0800

    Implemented help activity

--- app/src/main/java/com/example/video4kids/activities/HelpActivity.kt
+++ app/src/main/java/com/example/video4kids/activities/HelpActivity.kt
@@ -1,13 +1,27 @@
 package com.example.video4kids.activities
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.video4kids.R
+import com.example.video4kids.common.extensions.gotoAnotherActivity
+import com.mcxiaoke.koi.ext.onClick
+import kotlinx.android.synthetic.main.activity_help_screen.*
 
 class HelpActivity : AppCompatActivity() {
 
+    @SuppressLint("StringFormatInvalid")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help_screen)
+
+        helpContent.text = getString(R.string.user_details, R.string.perisan_user);
+        btnNext.text = getString(R.string.next, R.string.perisan_next);
+        helpContact.text = getString(R.string.contact_us, R.string.perisan_contact_us);
+        helpContactId.text = getString(R.string.deedani_ha, R.string.perisan_deedani_ha);
+
+        btnNext.onClick {
+            gotoAnotherActivity<AgeSelectionActivity>()
+        }
     }
 }
--- app/src/main/java/com/example/video4kids/common/extensions/Activity.kt
+++ app/src/main/java/com/example/video4kids/common/extensions/Activity.kt
@@ -2,6 +2,7 @@ package com.example.video4kids.common.extensions
 
 import android.app.Activity
 import android.content.Intent
+import com.example.video4kids.common.Pref
 import com.pawegio.kandroid.IntentFor
 import com.pawegio.kandroid.start
 
@@ -10,4 +11,8 @@ inline fun <reified T: Activity> Activity.gotoAnotherActivity() {
     intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK.or(Intent.FLAG_ACTIVITY_CLEAR_TOP)
     intent.start(this)
     finish()
+}
+
+fun Activity.getString(enResId: Int, prResId: Int): String {
+    return resources.getString(enResId.takeIf { Pref.isEnglish!! } ?: prResId)
 }
