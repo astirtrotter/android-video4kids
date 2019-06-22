41eb32a2f2f12de822300646fbb536cad0683b11
prodev9999 <prodev9999@gmail.com>
2019-06-22 21:44:56 +0800

    Implemented language selection activity

--- app/src/main/java/com/example/video4kids/activities/LanguageSelectionActivity.kt
+++ app/src/main/java/com/example/video4kids/activities/LanguageSelectionActivity.kt
@@ -3,6 +3,10 @@ package com.example.video4kids.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.video4kids.R
+import com.example.video4kids.common.Pref
+import com.example.video4kids.common.extensions.gotoAnotherActivity
+import com.mcxiaoke.koi.ext.onClick
+import kotlinx.android.synthetic.main.activity_language_screen.*
 
 class LanguageSelectionActivity : AppCompatActivity() {
 
@@ -10,6 +14,20 @@ class LanguageSelectionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_language_screen)
 
-
+        val navigate = {
+            if (Pref.isFirstLaunch) {
+                gotoAnotherActivity<HelpActivity>()
+            } else {
+                gotoAnotherActivity<MainActivity>()
+            }
+        }
+        langPersian.onClick {
+            Pref.isEnglish = false
+            navigate();
+        }
+        langEnglish.onClick {
+            Pref.isEnglish = true
+            navigate()
+        }
     }
 }
