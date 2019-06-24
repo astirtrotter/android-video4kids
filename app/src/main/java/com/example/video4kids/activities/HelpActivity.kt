79f15707b2fc7243112724663601676c490f8ed4
prodev9999 <prodev9999@gmail.com>
2019-06-24 22:41:38 +0800

    minor update

--- app/src/main/java/com/example/video4kids/activities/HelpActivity.kt
+++ app/src/main/java/com/example/video4kids/activities/HelpActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.video4kids.R
 import com.example.video4kids.activities.interfaces.IMultiLangScreen
+import com.example.video4kids.common.Pref
 import com.example.video4kids.common.extensions.getMultiLangString
 import com.example.video4kids.common.extensions.gotoAnotherActivity
 import com.mcxiaoke.koi.ext.onClick
@@ -26,7 +27,11 @@ class HelpActivity : AppCompatActivity(), IMultiLangScreen {
 
     override fun configureMultiLangViews() {
         helpContent.text = getMultiLangString(R.string.user_details, R.string.perisan_user);
-        btnNext.text = getMultiLangString(R.string.next, R.string.perisan_next);
+        if (Pref.isFirstLaunch) {
+            btnNext.text = getMultiLangString(R.string.next, R.string.perisan_next);
+        } else {
+            btnNext.text = getMultiLangString(R.string.ok, R.string.perisan_ok);
+        }
         helpContact.text = getMultiLangString(R.string.contact_us, R.string.perisan_contact_us);
         helpContactId.text = getMultiLangString(R.string.deedani_ha, R.string.perisan_deedani_ha);
     }
