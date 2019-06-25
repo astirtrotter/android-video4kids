5132c8e99b7361d47fe55f35cd0c5561226a2dd3
prodev9999 <prodev9999@gmail.com>
2019-06-25 14:17:13 +0800

    Added related videos activity

---
+++ app/src/main/java/com/example/video4kids/activities/RelatedVideosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.video4kids.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RelatedVideosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
